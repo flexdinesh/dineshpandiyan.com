175e956186a273ba34f734556c840048787867b0
Dinesh Pandiyan <flexdinesh@gmail.com>
2023-01-02 14:48:22 +1100

    refactor frontmatter schema

--- src/lib/markdoc/frontmatter.schema.ts
+++ src/lib/markdoc/frontmatter.schema.ts
@@ -1,18 +1,20 @@
 import { z } from "zod";
 
-export const label = z.optional(z.enum([
-  "twitter",
-  "github",
-  "npm",
-  "dev.to",
-  "medium",
-  "youtube",
-  "podcast",
-  "deprecated",
-  "archived",
-]));
+export const label = z.optional(
+  z.enum([
+    "twitter",
+    "github",
+    "npm",
+    "dev.to",
+    "medium",
+    "youtube",
+    "podcast",
+    "deprecated",
+    "archived",
+  ])
+);
 
-const blogBase = z.object({
+const baseSchema = z.object({
   draft: z.boolean().default(false),
   featured: z.boolean().default(false),
   title: z.string({
@@ -24,23 +26,7 @@ const blogBase = z.object({
     invalid_type_error:
       "date must be written in yyyy-mm-dd format without quotes: For example, Jan 22, 2000 should be written as 2000-01-22.",
   }),
-  label
-});
-
-const blogMarkdown = blogBase.extend({
-  external: z.literal(false),
-  description: z.optional(z.string()),
-  ogImagePath: z.optional(z.string()),
-  canonicalUrl: z.optional(z.string()),
-});
-
-const blogExternal = blogBase.extend({
-  external: z.literal(true),
-  url: z.string({
-    required_error:
-      "external is true but url is missing. url must be set for posts marked as external.",
-    invalid_type_error: "external should be string.",
-  }),
+  label,
 });
 
 /*
@@ -53,89 +39,57 @@ const blogExternal = blogBase.extend({
   just export blogMarkdown as your blog schema.
 */
 export const blog = z.discriminatedUnion("external", [
-  blogMarkdown,
-  blogExternal,
+  // markdown
+  baseSchema.extend({
+    external: z.literal(false),
+    description: z.optional(z.string()),
+    ogImagePath: z.optional(z.string()),
+    canonicalUrl: z.optional(z.string()),
+  }),
+  // external link
+  baseSchema.extend({
+    external: z.literal(true),
+    url: z.string({
+      required_error:
+        "external is true but url is missing. url must be set for posts marked as external.",
+      invalid_type_error: "external should be string.",
+    }),
+  }),
 ]);
 
-export const work = z.object({
-  draft: z.boolean().default(false),
+export const work = baseSchema.extend({
   external: z.literal(true).default(true),
-  featured: z.boolean().default(false),
-  title: z.string({
-    required_error: "Required frontmatter missing: title",
-    invalid_type_error: "title must be a string",
-  }),
-  date: z.date({
-    required_error: "Required frontmatter missing: date",
-    invalid_type_error:
-      "date must be written in yyyy-mm-dd format without quotes: For example, Jan 22, 2000 should be written as 2000-01-22.",
-  }),
   url: z.string(),
-  label
-});
-
-const notesBase = z.object({
-  draft: z.boolean().default(false),
-  featured: z.boolean().default(false),
-  title: z.string({
-    required_error: "Required frontmatter missing: title",
-    invalid_type_error: "title must be a string",
-  }),
-  date: z.date({
-    required_error: "Required frontmatter missing: date",
-    invalid_type_error:
-      "date must be written in yyyy-mm-dd format without quotes: For example, Jan 22, 2000 should be written as 2000-01-22.",
-  }),
-  label
-});
-
-const notesMarkdown = notesBase.extend({
-  external: z.literal(false),
-});
-
-const notesExternal = notesBase.extend({
-  external: z.literal(true),
-  url: z.string({
-    required_error:
-      "external is true but url is missing. url must be set for notes marked as external.",
-    invalid_type_error: "external should be string.",
-  }),
 });
 
 export const notes = z.discriminatedUnion("external", [
-  notesMarkdown,
-  notesExternal,
+  // markdown
+  baseSchema.extend({
+    external: z.literal(false),
+  }),
+  // external link
+  baseSchema.extend({
+    external: z.literal(true),
+    url: z.string({
+      required_error:
+        "external is true but url is missing. url must be set for notes marked as external.",
+      invalid_type_error: "external should be string.",
+    }),
+  }),
 ]);
 
-const talkBase = z.object({
-  draft: z.boolean().default(false),
-  featured: z.boolean().default(false),
-  title: z.string({
-    required_error: "Required frontmatter missing: title",
-    invalid_type_error: "title must be a string",
-  }),
-  date: z.date({
-    required_error: "Required frontmatter missing: date",
-    invalid_type_error:
-      "date must be written in yyyy-mm-dd format without quotes: For example, Jan 22, 2000 should be written as 2000-01-22.",
-  }),
-  label
-});
-
-const talkMarkdown = talkBase.extend({
-  external: z.literal(false),
-});
-
-const talkExternal = talkBase.extend({
-  external: z.literal(true),
-  url: z.string({
-    required_error:
-      "external is true but url is missing. url must be set for talks marked as external.",
-    invalid_type_error: "external should be string.",
-  }),
-});
-
 export const talk = z.discriminatedUnion("external", [
-  talkMarkdown,
-  talkExternal,
+  // markdown
+  baseSchema.extend({
+    external: z.literal(false),
+  }),
+  // external link
+  baseSchema.extend({
+    external: z.literal(true),
+    url: z.string({
+      required_error:
+        "external is true but url is missing. url must be set for talks marked as external.",
+      invalid_type_error: "external should be string.",
+    }),
+  }),
 ]);
