7f99f03cb1708352fdc175eea0e5ee0a7dd2de8a
Dinesh Pandiyan <flexdinesh@gmail.com>
2023-01-03 22:22:51 +1100

    blockquote and slight color update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -58,13 +58,13 @@ module.exports = {
               position: "absolute",
               left: 0,
               content: '"»"',
-              color: "rgb(var(--color-primary-main))",
+              color: "rgb(var(--color-primary-muted))",
             },
             ol: {
               "padding-left": "1.125rem",
             },
             "ol > li::marker": {
-              color: "rgb(var(--color-primary-main))",
+              color: "rgb(var(--color-primary-muted))",
             },
             "h1, h2, h3, h4, h5": {
               color: "rgb(var(--color-text-heading))",
@@ -75,6 +75,36 @@ module.exports = {
             "code::after": {
               content: "none",
             },
+            blockquote: {
+              border: "none",
+              position: "relative",
+              width: "96%",
+              margin: "0 auto",
+              color: "rgb(var(--color-text-body))",
+              "font-size": "1.125em",
+              "padding-top": "1.5rem",
+              "padding-bottom": "1.5rem",
+              "padding-left": "1.5rem",
+              "padding-right": "1.5rem",
+            },
+            "blockquote::before": {
+              "font-family": "Arial",
+              content: "open-quote",
+              "font-size": "4em",
+              color: "rgb(var(--color-text-link))",
+              position: "absolute",
+              left: "-10px",
+              top: "-10px",
+            },
+            "blockquote::after": {
+              content: "",
+            },
+            "blockquote p:first-of-type::before": {
+              content: "",
+            },
+            "blockquote p:last-of-type::after": {
+              content: "",
+            },
           },
         },
         sleek: {
@@ -84,9 +114,9 @@ module.exports = {
             "--tw-prose-lead": "rgb(var(--color-text-body))",
             "--tw-prose-links": "rgb(var(--color-text-body))",
             "--tw-prose-bold": "rgb(var(--color-text-bold))",
-            "--tw-prose-counters": "rgb(var(--color-primary-main))",
-            "--tw-prose-bullets": "rgb(var(--color-primary-main))",
-            "--tw-prose-hr": "rgb(var(--color-primary-main))",
+            "--tw-prose-counters": "rgb(var(--color-primary-muted))",
+            "--tw-prose-bullets": "rgb(var(--color-primary-muted))",
+            "--tw-prose-hr": "rgb(var(--color-primary-muted))",
             "--tw-prose-quotes": "rgb(var(--color-text-body))",
             "--tw-prose-quote-borders": "rgb(var(--color-primary-main))",
             "--tw-prose-captions": "rgb(var(--color-primary-heading))",
